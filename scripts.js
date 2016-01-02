080ac78ef3469fefbb3a46a0657853fa91c28051
zoejf <zoejfoss@gmail.com>
2016-01-02 15:16:50 -0800

    fix input, data-section hides/shows

--- scripts.js
+++ scripts.js
@@ -1,26 +1,54 @@
 $(function() {
     console.log( "ready!" );
 
-     var myFirebaseRef = new Firebase("https://radiant-fire-16.firebaseio.com/");
+    var myFirebaseRef = new Firebase("https://radiant-fire-16.firebaseio.com/");
+
+    // var $scroll = $('.messages-row');
+    // var height = $scroll[0].scrollHeight;
+    // $scroll.scrollTop(height);
+    $('.messages-row').animate({ 'scrollTop': $('.messages-row')[0].scrollHeight}, '2000');
+    
+
+    $('.glyphicon.navbar-link').on('click', function() {
+        $('.data-section').toggleClass('show');
+        $('.data-section').toggleClass('col-sm-4');
+        $('.messages-section').toggleClass('col-sm-12');
+        $('.messages-section').toggleClass('col-sm-8');
+    });
 
     function getUsername() {
     	//get username from Local Storage or prompt user
     	var username = localStorage.getItem("username");
 
     	if (username === null) {
-    		username = prompt("What is your username?");
-    		localStorage.setItem("username", username);
-    	}
+    		$('#myModal').modal('show');
+            $('.username-form').on('submit', function(){
+                username = $('#username').val();
+                localStorage.setItem("username", username);
+                $('#myModal').modal('hide');
+            });
+    	} else if (username == 'null') {
+            localStorage.setItem("username", "anonymous_panda");
+            username = "anonymous_panda";   
+        }
     	return username;
     }
     getUsername();
 
     $('.chat-form').on('submit', function() {
-    	console.log('submitted:', $('#message').val());
+    	var username = getUsername() || "anonymousss";
+        //message is getting sent to Firebase before username is ready??
     	myFirebaseRef.push({
-    		username: getUsername(), 
+    		username: username, 
     		message: $('#message').val()
     	});
+
+        //put username on page and give option to change
+        //!! doesn't quite work yet!!
+        $('<p />', {
+            'class': 'chatting-as',
+            text: 'Chatting as' + username + '<a data-toggle="modal" data-target="#myModal">[change]</a>'
+        }).appendTo($('.input-row'));
     });
 
     myFirebaseRef.on("child_added", createMessageFromFirebase);

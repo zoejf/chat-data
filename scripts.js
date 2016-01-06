cf2a469ec36c1d3af26a2670438e6abdc3a968ae
zoejf <zoejfoss@gmail.com>
2016-01-06 11:49:45 -0800

    gets sentiment when new message is posted

--- scripts.js
+++ scripts.js
@@ -8,33 +8,34 @@ $(function() {
     var $messages = $('#messages-list');
     var messageCount = 0;
 
-    //constructor function for a message
-    //add sentiment later
-    function Messages (message, author, createdAt) {
+    function Messages (message, author, createdAt, score, string) {
         this.message = message;
         this.author = author;
         this.createdAt = createdAt;
-        //this.sentiment = sentiment;
+        this.sentimentScore = score;
+        this.sentimentString = string;
     }
 
     Messages.all = [];
 
     function dateFormat(d) {
-        var hours = function() {
-            if (d.getHours() < 10) {
-                return "0" + d.getHours();
-            } else {
-                return d.getHours();
-            }
-        };
-        var minutes = function() {
-            if (d.getMinutes() < 10) {
-                return "0" + d.getMinutes();
-            } else {
-                d.getMinutes();
-            }
-        };
-        d = (d.getMonth()+1) + "/"+ d.getDate() + "/" + d.getFullYear() + " at " + hours + ":" + minutes;
+        // var hours = function hours() {
+        //     var h = d.getHours();
+        //     if (h < 10) {
+        //         h = "0" + h;
+        //         return h;
+        //     } else {
+        //         return h;
+        //     }
+        // };
+        // var minutes = function minutes() {
+        //     if (d.getMinutes() < 10) {
+        //         return "0" + d.getMinutes();
+        //     } else {
+        //         d.getMinutes();
+        //     }
+        // };
+        d = (d.getMonth()+1) + "/"+ d.getDate() + "/" + d.getFullYear() + " at " + d.getHours() + ":" + d.getMinutes();
         return d;
     }
 
@@ -58,7 +59,6 @@ $(function() {
 
 
     function getUsername() {
-    	//get username from Local Storage or prompt user
     	var username = localStorage.getItem("username");
 
     	if (username === null) {
@@ -83,16 +83,17 @@ $(function() {
 
     $('.chat-form').on('submit', function(event) {
         event.preventDefault();
-    	var username = getUsername();
-        // var sentiment = getSentiment();
-        //message is getting sent to Firebase before username is ready??
         var date = new Date();
         date = dateFormat(date);
-    	myFirebaseRef.push({
-    		username: username, 
-    		message: $('#message').val(), 
-            createdAt: date
-    	});
+        var message = {
+            username: getUsername(),
+            message: $('#message').val(),
+            createdAt: date, 
+            sentiment: []
+        };
+        console.log("message before sentiment", message);
+        setWithSentiment(message);
+    	
         $('.chat-form')[0].reset();
         scrollToBottom();
     });
@@ -105,9 +106,8 @@ $(function() {
     function getMessageFromFirebase(snapshot) {
         var messageData = snapshot.val();
         messageCount++;
-        // var sentiment = messageData.sentiment || getSentiment(messageData.message);
         var time = messageData.createdAt || (new Date().getTime());
-        var message = new Messages(messageData.message, messageData.username, time);
+        var message = new Messages(messageData.message, messageData.username, time, message.sentimentScore, sentimentString);
         message.save();
         message.render();
     }
@@ -115,36 +115,57 @@ $(function() {
 
 
     //---ALCHEMY SENTIMENT API--//
-    $('#update-data').on('click', function() {
-        var sentimentResult;
 
+    function setWithSentiment(message) {
+        var text = message.message.replace(/[.,-\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+        text = text.replace(/\s/g, "%20");
         $.ajax({
             url: 'http://gateway-a.watsonplatform.net/calls/text/TextGetTextSentiment',
             type: 'POST', 
             dataType: 'jsonp',
             contentType: 'json',
+            async: 'false',
             jsonpCallback: 'callback', 
             data: {
                 apikey:'cdbf2ddc186378fdc5bb377f10b0e5e4770702d2', 
-                text: 'hello%20this%20is%20a%20great%20test%20that%20might%20never%20work',
+                text: text,
                 showSourceText:1, 
                 jsonp:'callback', 
                 outputMode: 'json'
             },
             success: function (data) {
-                console.log(data);
-                var score = parseInt(data.docSentiment.score) || 0;
-                var type = data.docSentiment.type;
+              var scoreNum;
+              var typeString;
+              if (data.docSentiment) {
+                  scoreNum = parseFloat(data.docSentiment.score);
+                  typeString = data.docSentiment.type;
+              } else {
+                  scoreNum = 0;
+                  typeString = "neutral";
+              }
+              message.sentiment[0] = scoreNum;
+              message.sentiment[1] = typeString;
 
-                $('#sentiment').clear();
-
-                var sentiment = $('<p />', {
-                    'class': 'sentiment',
-                    text: 'Sentiment: ' + type + '  |  Score: ' + score
-                }).appendTo($('#sentiment'));
+              var newMessage = {
+                  username: message.username, 
+                  message: message.message, 
+                  createdAt: message.createdAt, 
+                  sentimentScore: message.sentiment[0],
+                  sentimentString: message.sentiment[1]
+              };
+              
+              myFirebaseRef.push(newMessage);
+              console.log('new message added: ', newMessage);
             } 
         });
-        
-    });
+    }
+
+    // $('#update-data').on('click', function() {
+    //     var totalSentiment = 0;
+    //     for(var i = 0; i < Messages.all.length; i++) {
+    //         var sentimentResult = getSentiment(Messages.all[i].message.toString());
+    //     }
+
+    // });
 
 });
